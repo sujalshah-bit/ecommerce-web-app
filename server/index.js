bfcf4ed0354e960a5607f3eaea2f3cf006a04167
Sujal Shah <sujalshah28092004@gmail.com>
2023-06-20 04:18:07 +0530

    add url origin url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin:'http://localhost:3000',
+  origin:'https://ecommerce-web-app-j9h3.vercel.app/',
   credentials:true
 }));
 app.use(cookieParser());
