8142f7f196ea5ba3947774ef24cf08a617733d99
Sujal Shah <sujalshah28092004@gmail.com>
2023-06-20 04:28:14 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin:'https://ecommerce-web-app-j9h3.vercel.app/',
-  credentials:true
-}));
+app.use(cors());
 app.use(cookieParser());
 
 // Connect to the database
